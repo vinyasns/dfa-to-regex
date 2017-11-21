7d75e343560d79609ed15d01f8a1c6f55050ec7d
Vinyas N S <vinyas.n.s@gmail.com>
2017-11-21 21:54:41 +0530

    create dfa.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-class DFA:
-
-    def __init__(self, states, alphabets, init_state, final_states, transition_matrix):
-        self.states = states
-        self.alphabets = alphabets
-        self.init_state = init_state
-        self.final_states = final_states
-        self.transition_matrix = transition_matrix
-
-    def toregex(self):
-        pass
+from .dfa import DFA
 
 
 def main():
@@ -20,6 +10,7 @@ def main():
     final_states = input('Enter the final states : ')
     final_states = final_states.split()
     transition_matrix = [list(map(str, input().split())) for _ in range(len(alphabets))]
+
     dfa = DFA(states, alphabets, init_state, final_states, transition_matrix)
     dfa.toregex()
 
